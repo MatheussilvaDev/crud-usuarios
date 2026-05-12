64b7f57fa856bcbd47bcc39c692d6ea8d85b8bba
MatheussilvaDev <matheushenrique2112@outlook.com>
2026-05-12 19:47:10 -0300

    Incluindo as classes do Spring Security e as dependencias do JJWT

---
+++ src/main/java/com/mathdev/usuario/infrastructure/entity/Endereco.java
@@ -0,0 +1,43 @@
+package com.mathdev.usuario.infrastructure.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "estado", length = 2)
+    private String estado;
+
+    @Column(name = "cidade", length = 100)
+    private String cidade;
+
+    @Column(name = "bairro", length = 100)
+    private String bairro;
+
+    @Column(name = "cep", length = 9)
+    private String cep;
+
+    @Column(name = "rua")
+    private String rua;
+
+    @Column(name = "numero", length = 10)
+    private int numero;
+
+    @Column(name = "complemento")
+    private String complemento;
+
+}
